a2dd7cd13ec440529cea167d40e0aa694587a9b3
leanringisfunny <qk12tkd@naver.com>
2022-06-25 15:47:12 +0900

    frontController

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -0,0 +1,40 @@
+package hello.servlet.web.frontcontroller.v1;
+
+import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name="frontControllerServletV1",urlPatterns="/front-controller/v1/*")
+//v1 하위에 존재하는 파일에 대해서는  무조건 이 컨트롤러가 호추리된다.
+public class FrontControllerServletV1 extends HttpServlet{
+    Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV1(Map<String, ControllerV1> controlletMap) {
+        controlletMap.put("/front-controller/v1/membrs/new-form",new MemberFormControllerV1() );
+        controlletMap.put("/front-controller/v1/membrs/save",new MemberSaveControllerV1());
+        controlletMap.put("/front-controller/v1/membrs",new MemberListControllerV1());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        ControllerV1 controllerV1 = controllerMap.get(requestURI);
+
+        if(controllerV1==null){
+            response.setStatus(response.SC_NOT_FOUND);
+            return;
+        }
+        //성공적으로 불러 왔으면 process호출->override된 서블릿 객체 실행
+        controllerV1.process(request,response);
+
+    }
+}
