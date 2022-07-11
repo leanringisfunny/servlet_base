a105e13d6be21b68ae94dc5471f8c31f7568cebc
leanringisfunny <qk12tkd@naver.com>
2022-07-11 14:54:01 +0900

    checkbox--multi

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,6 +20,16 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    //어떤 컨트롤러이든지 컨트롤러가 호출이되면 컨트롤러 내에서 spring에 의해 addAttribute가 자동으로 호출된다.// 계속 생성 커맨드를 사용하게 하면 성능상 효율이 떨어지므로, 따로 static저장 영역을 만들어서 저장하는것이 효울적일 수 있다.
+    @ModelAttribute("regions")
+    public Map<String ,String>region(){
+        Map<String,String> regions= new LinkedHashMap<>(); //걍 해시맵을 쓰면 순서를 보장받지 못하므로 고정된 순서표현을 위해 linked hash map을 쓴다.
+        regions.put("SEOUL","서울");
+        regions.put("BUSAN","부산");
+        regions.put("JEJU","제주");
+        return regions;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -30,6 +42,8 @@ public class FormItemController {
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
+
         return "form/item";
     }
 
@@ -45,6 +59,7 @@ public class FormItemController {
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}",item.getOpen());
+        log.info("item.regions={}",item.getRegions());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -55,6 +70,7 @@ public class FormItemController {
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
         return "form/editForm";
     }
 
