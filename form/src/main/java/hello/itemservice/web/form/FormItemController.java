9d6407ab7b5ff5fc1cfce34e8c66410f97d76cb7
leanringisfunny <qk12tkd@naver.com>
2022-07-11 18:31:03 +0900

    radio button and select box

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,7 +1,9 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +32,22 @@ public class FormItemController {
         regions.put("BUSAN","부산");
         regions.put("JEJU","제주");
         return regions;
+    }
+    //ItemTypes.valuse()를 호출 시 enum 값들을 배열로 넘겨준다
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+        return ItemType.values();
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCode(){
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+
+        deliveryCodes.add(new DeliveryCode("FAST","빠른배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW","느린배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL","보통배송"));
+
+        return deliveryCodes;
     }
 
     @GetMapping
@@ -60,6 +79,8 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}",item.getOpen());
         log.info("item.regions={}",item.getRegions());
+        log.info("item.ItemType={}",item.getItemType());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
