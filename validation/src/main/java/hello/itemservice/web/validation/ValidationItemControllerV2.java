eb66def7343733090837cd1cb01bf324f4ae4813
leanringisfunny <qk12tkd@naver.com>
2022-07-14 16:20:29 +0900

    add Item mechanism 3 send messages using errors.properties and FeildError object or ObjectError object

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -17,6 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 @Controller
 @RequestMapping("/validation/v2/items")
 @RequiredArgsConstructor
@@ -94,7 +95,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         //스프링에서 에러를 관리할 수 있는 매커니즘인 BindingResult 제공
 
@@ -117,6 +118,47 @@ public class ValidationItemControllerV2 {
             if(totalPrice<10000){
                 //글로벌 오류는 필드값이 존재하지 않기 때문에 ObjectError 객체를 생성하여 관리한다.
                 bindingResult.addError(new ObjectError("item",null,null,"총 주문액은 1만원 이상이어야 함시다.,현재 값"+totalPrice));
+                //item은 modelAttribute에 담기는 이름
+            }
+        }
+
+        //검증 입력 실패시 다시 입력 폼으로 이동
+        if(bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
+            //bindingResult는 자동으로 뷰로 함꼐 넘어감
+            return "validation/v2/addForm";
+        }
+        //성공 로직
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        //스프링에서 에러를 관리할 수 있는 매커니즘인 BindingResult 제공
+
+        //필드 단위의 에러를 처리할 수 이도록 객체를 스프링에서 제공
+        //검증 로직 :글자가 없다면 브라우저에 에러메시지 뿌려주기위해 검증 메시지 저장
+        if(!StringUtils.hasText(item.getItemName())){
+            bindingResult.addError(new FieldError("item","itemName",item.getItemName(),false,new String[]{"required.item.itemName"},null,null));
+        };
+        if(item.getPrice()==null ||item.getPrice()<1000 ||item.getPrice()>1000000){
+            bindingResult.addError(new FieldError("item","price",item.getPrice(),false,new String[]{"range.item.price"},new Object[]{1000,1000000},null));
+        }
+        if(item.getQuantity()==null|| item.getQuantity()>=9999){
+            bindingResult.addError(new FieldError("item","quantity",item.getQuantity(),false,new String[] {"max.item.quantity"},new Object[]{10000},null));
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+
+        if(item.getPrice()!=null && item.getQuantity()!=null){
+            int totalPrice=item.getPrice()*item.getQuantity();
+            if(totalPrice<10000){
+                //글로벌 오류는 필드값이 존재하지 않기 때문에 ObjectError 객체를 생성하여 관리한다.
+                bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"},new Object[]{10000,totalPrice},null));
                 //item은 modelAttribute에 담기는 이름
             }
         }
