c1b2039220d76be770b56b045d6a964cbf8e4cf4
leanringisfunny <qk12tkd@naver.com>
2022-07-18 17:24:12 +0900

    obbject error handing in two direction

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,14 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;  //하이버네이트 자체 제공
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;   //BeanValidation에서 기본으로 제공, 모든 구현체에서 같은 구현체를 사용하게 된다.
 import javax.validation.constraints.NotNull;
 
 @Data
+//권징x//@ScriptAssert(lang="javascript" ,script="_this.quantity * _this.price>=10000",message ="총합이 10000원 넘게 입력해주세요")//provided by hibernate  objectError
 public class Item {
 
     private Long id;
--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -52,7 +52,18 @@ public class ValidationItemControllerV3 {
     @PostMapping("/add")
     //org.springframework.boot:spring-web-starte-validation 추가시,spring이 자동으로 GlobalValidatorFactoryBean(검증기)를 등록해
     // @Validated 애노테이션(검증기 사용을 위해 무조건 필요)으로 자동으로 bean validation(데이터의 애너테이션)이 수행이 된다.
-    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+
+        if(item.getPrice()!=null && item.getQuantity()!=null){
+            int totalPrice=item.getPrice()*item.getQuantity();
+            if(totalPrice<10000){
+                //글로벌 오류는 필드값이 존재하지 않기 때문에 ObjectError 객체를 생성하여 관리한다.
+
+                bindingResult.reject("totalPriceMin",new Object[]{10000,totalPrice},null);
+                //item은 modelAttribute에 담기는 이름
+            }
+        }
 
         //검증 입력 실패시 다시 입력 폼으로 이동(에러 생기면)
         if(bindingResult.hasErrors()){
